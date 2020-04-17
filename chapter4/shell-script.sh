5c7c08e0498ffb33b23796b110ac99c666963b5a
kotireddy555 <kotireddy555@gmail.com>
2020-04-17 11:48:29 +0100

    @cleanup

--- chapter4/shell-script.sh
+++
@@ -1,9 +0,0 @@
-#Install Apache
-yum install --quiet -y httpd httpd-devel
-#Copy configuration files.
-cp httpd.conf /etc/httpd/conf/httpd.conf
-cp httpd-vhosts.conf /etc/httpd/conf/httpd-hosts
-# Start Apache and configure it to run at boot
-service httpd start
-chkcong httpd on
-
